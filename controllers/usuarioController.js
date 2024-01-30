269f9d02be4d4571d8f1f527ba3d7e0a6d64f2ef
Pedro Rocha <phr123silva@gmail.com>
2024-01-30 10:06:04 -0300

    criar usuário

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,5 +3,22 @@ const db = require('../models/Usuario.js');
 const Usuario = db.Usuario;
 
 exports.createUsuario = async (req, res) => {
-    
+    let usuario = {};
+
+    try{
+        usuario.id = req.body.id;
+        usuario.login = req.body.login;
+        usuario.senha = req.body.senha;
+        usuario.codigoExclusao = req.body.codigoExclusão;
+        Usuario.create(usuario, 
+                { attributes: ['id', 'login', 'senha', 'codigoExclusao']})
+            .then(result => {
+                res.status(200).json(result);
+            });
+    } catch(error){
+        res.status(500).json({
+            message: "Erro ao criar usuário.",
+            error: error.message
+        });
+    }
 }
